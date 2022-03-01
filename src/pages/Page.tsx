adf7ec5446288b2ca7aa51957eb68c31200a12cc
Christopher Fiallos <fialloschris1@gmail.com>
2022-03-01 15:43:11 -0600

    Fixed home route. Added dynamic functionality for add notes and add todos.

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,20 +1,36 @@
 import {
   IonButton,
   IonButtons,
+  IonCol,
   IonContent,
   IonFab,
   IonFabButton,
   IonHeader,
   IonIcon,
   IonInput,
+  IonItem,
+  IonItemDivider,
+  IonLabel,
+  IonList,
   IonMenuButton,
   IonModal,
   IonPage,
+  IonPopover,
+  IonRadio,
+  IonRadioGroup,
+  IonRow,
+  IonSegment,
+  IonSegmentButton,
+  IonSelect,
+  IonSelectOption,
   IonTextarea,
   IonTitle,
+  IonToggle,
   IonToolbar
 } from '@ionic/react';
-import { add } from 'ionicons/icons';
+import {
+  add
+} from 'ionicons/icons';
 import { useState } from 'react';
 import { useParams } from 'react-router';
 import { useDispatch, useSelector } from 'react-redux';
@@ -71,8 +87,9 @@ interface iAddModalProps {
 
 const AddModal: React.FC<iAddModalProps> = ({ isModalOpen, nameParam }) => {
   const dispatch = useDispatch();
-  const [ title, setTitle ] = useState<any>();
-  const [ description, setDescription ] = useState<any>();
+  const [ title, setTitle ] = useState<string>('');
+  const [ description, setDescription ] = useState<string>('');
+  const [ task, setTask ] = useState<string>('');
 
   return (
     <IonModal isOpen={isModalOpen}>
@@ -93,21 +110,72 @@ const AddModal: React.FC<iAddModalProps> = ({ isModalOpen, nameParam }) => {
           </IonButtons>
         </IonToolbar>
       </IonHeader>
-      <IonContent>
-        <IonHeader>
-          <IonToolbar>
-            <IonInput
-              placeholder='Title'
-              value={title}
-              onIonChange={(e) => setTitle(e.detail.value)}/>
-          </IonToolbar>
-        </IonHeader>
+      { nameParam === 'notes' &&
+        <AddNoteModalContentProps
+          title={title}
+          setTitle={setTitle}
+          description={description}
+          setDescription={setDescription}/>}
+      { nameParam === 'todos' &&
+        <AddTodoModalContentProps
+          task={task}
+          setTask={setTask}/>}
+    </IonModal>
+  );
+};
+
+interface iAddNoteModalContentProps {
+  title: string
+  setTitle: (title: string) => void
+  description: string
+  setDescription: (description: string) => void
+};
+
+const AddNoteModalContentProps: React.FC<iAddNoteModalContentProps> = ({ title, setTitle, description, setDescription }) => {
+  const [ popupOpen, setPopupOpen ] = useState<boolean>(false);
+
+  return (
+    <IonContent>
+      <IonHeader >
+        <IonToolbar>
+          <IonInput
+            placeholder='Title'
+            value={title}
+            onIonChange={e => setTitle(e.detail.value!)}/>
+        </IonToolbar>
+      </IonHeader>
+      <IonItem lines='none'>
         <IonTextarea
           autoGrow
           autofocus
           value={description}
-          onIonChange={(e) => setDescription(e.detail.value)}/>
-      </IonContent>
-    </IonModal>
+          onIonChange={e => setDescription(e.detail.value!)}/>
+      </IonItem>
+    </IonContent>
+  );
+};
+
+interface iAddTodoModalContentProps {
+  task: string
+  setTask: (task: string) => void
+}
+
+const AddTodoModalContentProps: React.FC<iAddTodoModalContentProps> = ({ task, setTask }) => {
+  return (
+    <IonContent>
+      <IonHeader>
+        <IonToolbar>
+          <IonInput
+            placeholder='Todo'
+            value={task}
+            onIonChange={e => setTask(e.detail.value!)}/>
+        </IonToolbar>
+      </IonHeader>
+      <IonItemDivider />
+      <IonItem>
+        <IonLabel position='fixed'>Important</IonLabel>
+        <IonToggle slot='end'/>
+      </IonItem>
+    </IonContent>
   );
 };
